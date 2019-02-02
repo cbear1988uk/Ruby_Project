bc15500ef21c78c6eaae121c889a54ef27d0466b
Collin Bull <vhiessumaniac@hotmail.com>
2019-02-02 19:32:18 +0000

    added MORE views, fixed undefined method errors

--- animal_shelter_start_point/controller.rb
+++ animal_shelter_start_point/controller.rb
@@ -4,6 +4,7 @@ require('pry')
 require_relative('./models/owner')
 require_relative('./models/animal')
 require_relative('./models/adoptions')
+
 also_reload('./models/*')
 
 get '/' do
@@ -15,16 +16,27 @@ get '/animals/' do
   erb(:animals)
 end
 
+get '/owners/' do
+  @owners = Owner.find_all()
+  erb(:owners)
+end
+
 get '/animals/:id' do
   @animals = Animal.find_by_id(params[:id])
   erb(:show_animal)
+end
+
+get '/edit_animal/:id' do
+  @animals = Animal.find_by_id(params[:id])
+  erb(:edit_animal)
 end
 
 get '/donate_pet/' do
   erb(:new)
 end
 
-get '/adopt_pet/' do
+get '/adopt_pet/:id' do
+  @animals = Animal.find_by_id(params[:id])
   erb(:adopt)
 end
 
@@ -32,4 +44,14 @@ post '/animals/' do
   animals = Animal.new(params)
   animals.save()
   redirect '/animals/'
+end
+
+post '/animals/:id/delete' do
+ @animal = Animal.find(params['id'])
+ @animal.delete
+ redirect to '/animals/'
+end
+
+get '/edit_animal/' do
+  erb(:edit_animal)
 end
